a710ed341206e4e6199e0d91d98b80c8408b3c50
Gururaj Kulkarni <gururajk29@gmail.com>
2023-11-27 15:25:15 -0700

    Added task 2

--- Clustering/dbscan.py
+++ Clustering/dbscan.py
@@ -8,14 +8,14 @@ class DBScan:
     '''
         implements DBScan algorithm and also gives C significant clusters
     '''
-    def __init__(self, data, c, min_samples, eps=10):
+    def __init__(self, data, c, min_samples=2, eps=2.4, distances_df=None):
         self.c = c
         self.min_samples = min_samples
         self.eps = eps
-        self.clusters = self.__dbscan(data)
+        self.clusters = self.__dbscan(data, distances_df)
         self.centroids = self.__calculateCentroids(data)
     
-    def __dbscan(self, data):
+    def __dbscan(self, data, distances_df):
         cluster_labels = {i:0 for i in data}  # 0 denotes an undefined label
         cluster_id = 0  # Start with cluster_id as 1, as 0 is used for undefined
 
@@ -23,7 +23,7 @@ class DBScan:
             if cluster_labels[point_idx] != 0:  # Ignore already processed points
                 continue
 
-            neighbors = self.__getNeighbors(data, point_idx, self.eps)
+            neighbors = self.__getNeighbors(data, point_idx, distances_df)
 
             # If the point is not a core point, mark as noise (temporarily)
             if len(neighbors) < self.min_samples:
@@ -33,19 +33,21 @@ class DBScan:
                 cluster_id += 1
                 # Start expanding cluster from the core point
                 self.__growCluster(
-                    data, cluster_labels, point_idx, neighbors, cluster_id, self.eps, self.min_samples
+                    data, cluster_labels, point_idx, neighbors, cluster_id, distances_df
                 )
 
         return cluster_labels
     
-    def __getNeighbors(self, data, point_idx, eps):
+    def __getNeighbors(self, data, point_idx, distances_df):
+        if distances_df is not None:
+            return list(map(lambda x: x[0], filter(lambda x: x[0] != point_idx and x[1] < self.eps, zip(distances_df.index, distances_df.loc[point_idx]))))
         neighbors = []
         for idx, current_point in data.items():
-            if util.euclidean_distance(data[point_idx], current_point) < eps:
+            if idx != point_idx and util.euclidean_distance(data[point_idx], current_point) < self.eps:
                 neighbors.append(idx)
         return neighbors
 
-    def __growCluster(self, data, labels, point_idx, neighbors, cluster_id, eps, min_samples):
+    def __growCluster(self, data, labels, point_idx, neighbors, cluster_id, distances_df):
         # Label initial point as a core point of the cluster
         labels[point_idx] = cluster_id
         neighbors = deque(
@@ -57,9 +59,9 @@ class DBScan:
                 labels[neighbor_idx] = cluster_id  # Noise becomes edge point
             elif labels[neighbor_idx] == 0:  # Not yet visited
                 labels[neighbor_idx] = cluster_id  # Label as part of the cluster
-                neighbor_neighbors = self.__getNeighbors(data, neighbor_idx, eps)
+                neighbor_neighbors = self.__getNeighbors(data, neighbor_idx, distances_df)
                 # If neighbor is a core point, append its neighbors to the queue for further checking
-                if len(neighbor_neighbors) >= min_samples:
+                if len(neighbor_neighbors) >= self.min_samples:
                     for n_neighbor_idx in neighbor_neighbors:
                         if labels[n_neighbor_idx] <= 0:  # Not yet visited or noise
                             neighbors.append(n_neighbor_idx)
@@ -76,3 +78,6 @@ class DBScan:
     def getCSignicantClusters(self, point):
         most_significant_clusters = heapq.nlargest(self.c, self.centroids, key=lambda x: similarity_measures.l2_norm(point, self.centroids[x]))
         return most_significant_clusters
+
+    def getNumberOfClusters(self):
+        return len(self.centroids)
--- Tasks/task_2.py
+++ Tasks/task_2.py
@@ -1,6 +1,82 @@
-def Execute(arguments, db):
-    execute_internal(arguments, db)
+from Clustering import dbscan
+import util
+import pandas as pd
+from tqdm import tqdm
+import os
+import pathlib
 
-def execute_internal(arguments, db) :
-    # TODO
+TASK_ID = 2
+
+def Execute(arguments, db):
+    c = arguments["c"]
+    execute_internal(c, db)
+
+def execute_internal(c, db):
+    fd = util.get_feature_model(TASK_ID)
+    train_data = db.get_feature_descriptors(fd)
+    label_train_data = db.get_label_feature_descriptors(fd)
+    
+    cluster = dbscan.DBScan(train_data, c, distances_df = db.get_distances_matrix_df(fd))
+    number_of_clusters = cluster.getNumberOfClusters()
+    c = min(c, number_of_clusters) # if c is more than the number of clusters, then we can only use number_of_clusters
+
+    cluster_signifance_list = get_cluster_signifance(cluster, label_train_data, number_of_clusters, c)
+
+    test_data = db.get_feature_descriptors(fd, train_data=False)
+    test_labels = db.get_id_label_dict(train_data=False)
+
+    output_data = {"Image IDs":[], "Predicted Labels":[], "True Labels":[]}
+    predictions = {}
+    for image_id, image_vector in tqdm(test_data.items()):
+        predicted_label = predict_label(cluster, image_vector, label_train_data, cluster_signifance_list)
+        predictions[image_id] = predicted_label
+        output_data["Image IDs"].append(image_id)
+        output_data["Predicted Labels"].append(predicted_label)
+        output_data['True Labels'].append(test_labels.get(image_id))
+
+    util.AnalysePredictions(db, predictions)
+
+    pathlib.Path(util.Constants.TASK_2_LOCATION).mkdir(parents=True, exist_ok=True)
+    pd.DataFrame(output_data).set_index("Image IDs").to_csv(os.path.join(util.Constants.TASK_2_LOCATION, 'dbscan_%d.csv'%c))
     return
+
+def get_cluster_signifance(cluster, label_train_data, no_of_clusters, c):
+    '''
+        For each label vector, computes c significant cluster
+        Returns a list of dictionaries, each entry i contains a dictionary di which contains 
+        cluster (ci) -> labelIds pairs indicating that all these labels have ci as their ith significant cluster
+        labelIds are stored as sets for faster computations during predictions
+    '''
+    initiate_cluster_dicts = lambda x: {i:set() for i in range(1, x+1)}
+    cluster_significance = [initiate_cluster_dicts(no_of_clusters) for i in range(c)]
+
+    for label in label_train_data:
+        c_significant_clusters = cluster.getCSignicantClusters(label_train_data[label])
+        for i in range(len(c_significant_clusters)):
+            cluster_id = c_significant_clusters[i]
+            cluster_significance[i][cluster_id].add(label)
+
+    return cluster_significance
+
+
+def predict_label(cluster, image_vector, label_train_data, cluster_significance_list):
+    most_likely_labels = get_most_likely_labels(cluster, image_vector, label_train_data, cluster_significance_list)
+    return predict_label_from_most_likely_labels(image_vector, label_train_data, most_likely_labels)
+
+def get_most_likely_labels(cluster, image_vector, label_train_data, cluster_significance_list):
+    '''
+        Iterates over cluster significance list and returns the shortest non-empty set of labels which have same order cluster significance as the query image
+    '''
+    c_significant_clusters = cluster.getCSignicantClusters(image_vector)
+    likely_labels = set(label_train_data.keys())
+    for i in range(len(c_significant_clusters)):
+        next_label_labels = likely_labels & cluster_significance_list[i][c_significant_clusters[i]]
+        if len(next_label_labels) == 0: break
+        likely_labels = next_label_labels
+    return likely_labels
+
+def predict_label_from_most_likely_labels(image_vector, label_train_data, likely_labels):
+    '''
+        Uses euclidean distance to find the closest label vector
+    '''
+    return min(likely_labels, key = lambda x: util.euclidean_distance(image_vector, label_train_data[x]))
